4810d4bd47d6aa1c8b161aad3705121a94bbc1a6
salman g <salman444g@gamil.com>
2022-06-03 10:50:23 +0530

    push

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,12 +4,54 @@ import { useEffect,useState } from 'react';
 const Dashboard=()=>{
 
   const[getList,setList] =  useState([]);
+  const[getIndex,setIndex]=useState(-1);
+  const[getExpense,setExpense]=useState({
+    expenseName:'',
+    amount:'',
+    paidBy:'',
+    date:''
+  });
 
      useEffect(()=>{
             if(JSON.parse(sessionStorage.getItem('expenseDetails')) && JSON.parse(sessionStorage.getItem('expenseDetails')).length>0){
                setList(JSON.parse(sessionStorage.getItem('expenseDetails')))
             }
      },[])
+
+     const onDeleteHandler=(index)=>{
+       let expenseDetails = [...getList];
+       expenseDetails.splice(index,1);
+       setList(expenseDetails);
+       sessionStorage.setItem('expenseDetails',JSON.stringify(expenseDetails));
+     }
+
+     const onEditHandler=(index)=>{
+      setExpense({
+        expenseName:getList[index].expenseName,
+        amount:getList[index].amount,
+        paidBy:getList[index].paidBy,
+        date:getList[index].date
+      })
+      setIndex(index);
+     }
+
+     const onChangeHandler=(event)=>{
+      setExpense({
+        ...getExpense,[event.target.name]:event.target.value
+      })
+    }
+
+
+    const onEditSubmitHandler=(event)=>{
+      event.preventDefault();
+      let expenseDetails =[...getList];
+      expenseDetails[getIndex].expenseName = getExpense.expenseName;
+      expenseDetails[getIndex].amount=getExpense.amount;
+      expenseDetails[getIndex].paidBy = getExpense.paidBy;
+      expenseDetails[getIndex].date = getExpense.date;
+      setList(expenseDetails);
+      sessionStorage.setItem('expenseDetails',JSON.stringify(expenseDetails));
+    }
 
     return (<div>
        <div className="container-fluid">
@@ -54,8 +96,8 @@ const Dashboard=()=>{
                             <td>{obj.amount}</td>
                             <td>{obj.paidBy}</td>
                             <td>{obj.date}</td>
-                            <td><i  data-toggle="modal" data-target="#edit" className="fa fa-pencil-square-o" aria-hidden="true"></i></td>
-                            <td><i className="fa fa-trash" aria-hidden="true"></i></td>
+                            <td><i onClick={()=>onEditHandler(index)} data-toggle="modal" data-target="#edit" className="fa fa-pencil-square-o" aria-hidden="true"></i></td>
+                            <td><i onClick={()=>onDeleteHandler(index)} className="fa fa-trash" aria-hidden="true"></i></td>
                           </tr>
 
                            )
@@ -86,12 +128,30 @@ const Dashboard=()=>{
           </button>
         </div>
         <div className="modal-body">
-          ...
+        <form>
+                      <div className="form-group">
+                          <label>Expense Name</label>
+                          <input type="text"  value={getExpense.expenseName} onChange={onChangeHandler} name="expenseName" className="form-control" id="firstName"  placeholder="Enter first name"/>
+                        </div>
+                        <div className="form-group">
+                          <label>Amount</label>
+                          <input  value={getExpense.amount} onChange={onChangeHandler} type="text" name="amount" className="form-control" id="lastName"  placeholder="Enter last name"/>
+                        </div>
+                        
+                      <div className="form-group">
+                        <label>Paid By</label>
+                        <input value={getExpense.paidBy} onChange={onChangeHandler} type="text" name="paidBy" className="form-control" id="email" placeholder="Enter email"/>
+                      
+                      </div>
+                      <div className="form-group">
+                        <label>Date</label>
+                        <input value={getExpense.date} onChange={onChangeHandler} type="date"  name="date" className="form-control" id="password" placeholder="Password"/>
+                      </div>
+                  
+                      <button data-dismiss="modal" onClick={onEditSubmitHandler} type="submit" className="btn btn-success">ADD</button>
+                    </form>
         </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" className="btn btn-primary">Save changes</button>
-        </div>
+       
       </div>
     </div>
   </div>
